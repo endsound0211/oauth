e8c61a50f23c083756a2ee8e9c4ccc26aacadbf6
endsound <endsound0211@gmail.com>
2018-03-14 16:15:28 +0800

    log4j print jooq sql

--- db/src/main/java/com/endsound/jooq/JooqListener.java
+++ db/src/main/java/com/endsound/jooq/JooqListener.java
@@ -1,9 +1,18 @@
 package com.endsound.jooq;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jooq.ExecuteContext;
 import org.jooq.impl.DefaultExecuteListener;
 
 public class JooqListener extends DefaultExecuteListener {
+    private final static Logger logger = LogManager.getLogger(JooqListener.class);
+
+    @Override
+    public void executeStart(ExecuteContext ctx){
+        logger.debug(ctx.sql());
+    }
+
     @Override
     public void exception(ExecuteContext ctx){
         ctx.exception(new RuntimeException("database error", ctx.exception()));
